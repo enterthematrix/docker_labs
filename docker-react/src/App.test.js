18dfdc286036e53c1842081399d8e182ae82899c
sanju <sanjeev@streamsets.com>
2020-07-08 18:19:12 -0700

    simple docker project with a single docker file

---
+++ docker-react/src/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/I was changed on the feature branch/i);
+  expect(linkElement).toBeInTheDocument();
+})
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/I was changed on the feature branch/i);
+  expect(linkElement).toBeInTheDocument();
+});
